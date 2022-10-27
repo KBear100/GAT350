7963e757141e6775066370b290d67db367338399
KBear100 <kgamett@student.neumont.edu>
2022-10-27 16:20:47 -0600

    Per pixel shading

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -62,35 +62,6 @@ int main(int argc, char** argv)
 	// load scene
 	auto scene = Bear::g_resources.Get<Bear::Scene>("Scenes/basic_lit.scn");
 
-	// create shader
-	//std::shared_ptr<Bear::Shader> vs = Bear::g_resources.Get<Bear::Shader>("Shaders/basic.vert", GL_VERTEX_SHADER);
-	//std::shared_ptr<Bear::Shader> fs = Bear::g_resources.Get<Bear::Shader>("Shaders/basic.frag", GL_FRAGMENT_SHADER);
-
-	////create vertex buffer
-	//std::shared_ptr<Bear::VertexBuffer> vb = Bear::g_resources.Get<Bear::VertexBuffer>("box");
-	//vb->CreateVertexBuffer(sizeof(vertices), 36, vertices);
-	//vb->SetAttribute(0, 3, 8 * sizeof(float), 0);
-	////vb->SetAttribute(1, 3, 8 * sizeof(float), 3 * sizeof(float));
-	//vb->SetAttribute(1, 2, 8 * sizeof(float), 6 * sizeof(float));
-
-	////create material
-	//std::shared_ptr<Bear::Material> material = Bear::g_resources.Get<Bear::Material>("Materials/spot.mtrl");
-	//material->Bind();
-
-	////create model
-	//auto m = Bear::g_resources.Get<Bear::Model>("Models/spot.obj");
-	//
-	//glm::mat4 model{ 1 };
-	//glm::mat4 projection = glm::perspective(45.0f, Bear::g_renderer.GetWidth() / (float)Bear::g_renderer.GetHeight(), 0.01f, 100.0f);
-
-	//glm::vec3 cameraPosition{ 0, 0, 0 };
-
-	//std::vector<Bear::Transform> transforms;
-	//for (size_t i = 0; i < 1000; i++)
-	//{
-	//	transforms.push_back({ {Bear::randomf(-10, 10), Bear::randomf(-10, 10), Bear::randomf(-10, 10)}, {Bear::randomf(360), 90, 0} });
-	//}
-
 	bool quit = false;
 	while (!quit)
 	{
